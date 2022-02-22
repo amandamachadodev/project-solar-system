e921636d5bc98f5e48caece26868efedac5e3c13
Your Name <you@example.com>
2022-02-22 00:44:16 -0300

    Erros de lint corrigidos

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
-    const planetItem = `Planeta ${this.props.planetName}`;
+    const { planetName, planetImage } = this.props;
+    const planetItem = `Planeta ${planetName}`;
     return (
       <div data-testid="planet-card">
-        <p data-testid="planet-name">{this.props.planetName}</p>
-        <img src={ this.props.planetImage } alt={ planetItem } />
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ planetItem } />
       </div>
     );
   }
 }
 
+PlanetCard.propTypes = {
+  planetImage: PropTypes.string.isRequired,
+  planetName: PropTypes.string.isRequired,
+};
 export default PlanetCard;
